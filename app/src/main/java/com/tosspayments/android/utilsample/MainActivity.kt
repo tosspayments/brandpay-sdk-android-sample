efd38207866bbfaab3fe5956557f6b13e5441da1
kangdroid <kangdroid@toss.im>
2021-11-24 16:54:59 +0900

    - init project

---
+++ app/src/main/java/com/tosspayments/android/utilsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tosspayments.android.utilsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
