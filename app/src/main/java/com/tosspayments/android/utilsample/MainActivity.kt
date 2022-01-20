e8c059859b372bd79471538ca3ed0c2628de9ae2
Kangdroid <kangdroid@toss.im>
2022-01-20 18:41:48 +0900

    [CON-1803] [Android] getAppInfo 앱브릿지 추가 - getAppInfo 앱브릿지 추가

--- app/src/main/java/com/tosspayments/android/utilsample/MainActivity.kt
+++ app/src/main/java/com/tosspayments/android/utilsample/MainActivity.kt
@@ -1,28 +1,58 @@
 package com.tosspayments.android.utilsample
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import com.tosspayments.android.auth.utils.ConnectPayAuthManager
 import com.tosspayments.android.ocr.common.ConnectPayOcrManager
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var preference: SharedPreferences
+
     companion object {
         private const val REQUEST_CODE_CARD_SCAN = 1001
         private const val REQUEST_CODE_PASSWORD = 1002
+
+        private const val KEY_WEB_URL = "keyWebUrl"
     }
+
+    private val gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        preference = getSharedPreferences("keyConnectPaySamplePref", Context.MODE_PRIVATE)
 
         initViews()
     }
 
     private fun initViews() {
         val activity = this@MainActivity
+
+        val inputWebUrl = findViewById<EditText>(R.id.web_url).apply {
+            val webUrl = preference.getString(
+                KEY_WEB_URL,
+                null
+            ) ?: "https://demo-dev.tosspayments.com/connectpay/test/webview"
+
+            setText(webUrl)
+        }
+
+        findViewById<Button>(R.id.get_app_info).setOnClickListener {
+            startActivity(
+                ResultActivity.getIntent(
+                    activity,
+                    successData = gson.toJson(ConnectPayAuthManager.getAppInfo(this@MainActivity))
+                )
+            )
+        }
 
         findViewById<Button>(R.id.verify_biometric_auth).setOnClickListener {
             ConnectPayAuthManager.requestBioMetricAuth(activity,
